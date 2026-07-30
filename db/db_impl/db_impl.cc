d8cdc829c5701a0191134107a2f7527b6d772f6f
gengliqi <gengliqiii@gmail.com>
2026-07-30 11:19:52 +0800

    change to 3s
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- db/db_impl/db_impl.cc
+++ db/db_impl/db_impl.cc
@@ -5986,7 +5986,7 @@ Status DBImpl::IngestExternalFiles(
       }
       if (consumed_seqno_count > 0) {
         const SequenceNumber last_seqno = versions_->LastSequence();
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(3));
         versions_->SetLastAllocatedSequence(last_seqno + consumed_seqno_count);
         versions_->SetLastPublishedSequence(last_seqno + consumed_seqno_count);
         versions_->SetLastSequence(last_seqno + consumed_seqno_count);
